e52cae6fd0e88eb6dc368681eae02686d73a9d36
wangshuai09 <391746016@qq.com>
2024-09-10 01:11:10 +0000

    not compile _core_ext

--- setup.py
+++ setup.py
@@ -286,7 +286,7 @@ def _build_custom_ops() -> bool:
 
 
 def _build_core_ext() -> bool:
-    return not (_is_neuron() or _is_tpu() or _is_openvino() or _is_xpu())
+    return not (_is_neuron() or _is_tpu() or _is_openvino() or _is_xpu() or _is_npu())
 
 
 def get_hipcc_rocm_version():
